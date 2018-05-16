5bcbe25bcad056537ad2d88105f5c514bb527802
Eliska <eliska.achee@gmail.com>
2018-05-16 17:15:09 -0600

    Divided dates into calendar events, accounting for marriage events.

--- routes/calendar.js
+++ routes/calendar.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 var restError = require('../lib/rest-error');
 var async = require('async');
+var util = require('util');
+// Holds the calendar dates
+var calendar = [
+  {'monthName': 'January', dates:{}},
+  {'monthName': 'February', dates:{}},
+  {'monthName': 'March', dates:{}},
+  {'monthName': 'April', dates:{}},
+  {'monthName': 'May', dates:{}},
+  {'monthName': 'June', dates:{}},
+  {'monthName': 'July', dates:{}},
+  {'monthName': 'August', dates:{}},
+  {'monthName': 'September', dates:{}},
+  {'monthName': 'October', dates:{}},
+  {'monthName': 'November', dates:{}},
+  {'monthName': 'December', dates:{}}
+];
 
 // Setup the FS sdk client before handling any requests on this router.
 router.use(require('../middleware/fs-client'));
@@ -9,102 +25,101 @@ router.use(require('../middleware/fs-client'));
 // Make sure the user is signed in before handling any requests on this router.
 router.use(require('../middleware/fs-session'));
 
-// When /pedigree is requested, get the user's personId from the session
-// and redirect to /pedigree/[personId]. Not only does this allow us to have
-// only one route handler for the pedigree but it also makes the URL easier
-// for the user to understand and share.
 router.get('/', function(req, res){
   res.redirect('/calendar/' + req.session.user.personId);
 });
 
-// Download a person's 2-generation pedigree and a list of their children. Then
-// fetch portrait URLs for all those people.
+function isValid(dateString) {
+  
+  var splitDateString = dateString.split(" ");
+  if(!splitDateString[0] || !splitDateString[1] || !splitDateString[2]) {
+    return false;
+  }
+  return true;
+}
+/**
+Organizes the events by date and pushes them into the calendar, indexed by
+day inside the proper month.
+*/
+function addEventToCalendar(name, date, ascendancyNumber, gender, type) {
+  console.log(name + " " + date + " " + type);
+  var eventDate = new Date(date);
+  var eventInfo = {
+    'name': name,
+    'gender': gender,
+    'ascendancyNumber': ascendancyNumber,
+    'day': eventDate.getDate(),
+    'month': eventDate.getMonth(),
+    'year': eventDate.getFullYear(),
+    'dayOfTheWeek': eventDate.getDay(), 
+    'type': type
+  };
+  
+  var dateString = ' ' + eventInfo.day;
+  if(!calendar[eventDate.getMonth()].dates[dateString]) {
+    calendar[eventDate.getMonth()].dates[dateString] = [];
+    calendar[eventDate.getMonth()].dates[dateString].push(eventInfo);
+  } else {
+    // if there is already a marriage event in the calendar for their spouse
+    if(type === "marriage") {
+      for (var event in calendar[eventDate.getMonth()].dates[dateString]) {
+        // if there is a match
+        if ((Math.floor(calendar[eventDate.getMonth()].dates[dateString][event].ascendancyNumber / 2) === Math.floor(ascendancyNumber / 2)) && (calendar[eventDate.getMonth()].dates[dateString][event].type === "marriage")){
+          calendar[eventDate.getMonth()].dates[dateString][event].name += (" and " + name);
+        } else {
+          calendar[eventDate.getMonth()].dates[dateString].push(eventInfo);
+        }
+      }
+    } else { // it is a birth or death event
+      calendar[eventDate.getMonth()].dates[dateString].push(eventInfo);
+    }
+  }
+}
+
 router.get('/:personId', function(req, res, next) {
 
   var fs = req.fs,
       personId = req.params.personId;
 
-  // http://caolan.github.io/async/docs.html#autoInject
-  // async.autoInject is an easy way for us to specify and manage dependencies
-  // between asynchronous tasks.
+  // manages dependancies between asyncronous tasks (results are name of task)
   async.autoInject({
 
-    // Fetch the person's ancestry. We ask for 2 generations which includes the
-    // root person, their parents, and their grandparents (meaning the generations
-    // parameter doesn't count the root person as a generation).
-    dates: function(callback){
-
-      // https://familysearch.org/developers/docs/api/tree/Ancestry_resource
+    calendar: function(callback){
       fs.get('/platform/tree/ancestry?generations=3&person=' + personId + "&personDetails=true&marriageDetails=true", function(error, response){
-
-        // When requesting a person's ancestry, anything other than an HTTP 200
-        // is unexpected and thus we treat it as an error.
-        //
-        // When `error` is set we won't have a response but the code below will
-        // work regardless due to short-circuit evaluation.
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Short-circuit_evaluation
         if(error || response.statusCode !== 200){
           return callback(error || restError(response));
         }
-
-        // Persons returned by the Ancestry resource will include an
-        // ascendancyNumber number in their display block. This is an ahnentafel
-        // number (Google it) that tells us the person's position in the
-        // pedigree. To make the pedigree display easy and fast, we add the
-        // people to a map keyed by that ahnentafel number. Then the pedigree
-        // template can quickly load a person for a position by looking to see
-        // if that position's number exists as a key in the ancestry map.
-        // This is a O(n) operation.
-        //
-        // Alternatives include looping through the list each time you want to
-        // load a person for a given position, wich is O(n^2), or assembling
-        // the pedigree in memory in a nested structure and then recursively
-        // traversing that structure in the template (it's as complicated as it
-        // sounds).
-        // var ancestry = {};
-        // response.data.persons.forEach(function(person){
-        //   console.log("PERSON: ", person);
-        //   ancestry[person.display.ascendancyNumber] = person;
-        // });
-        var dates = [];
         response.data.persons.forEach(function(person) {
-          // console.log("PERSON: ", person.display.name);
           //Get birth date
           if(person.display.birthDate) {
-            // console.log("Birth Date: " + new Date(person.display.birthDate));
-            dates.push({'name': person.display.name, 'date': new Date(person.display.birthDate), 'type': 'birth'});
+            if(isValid(person.display.birthDate)) {
+              addEventToCalendar(person.display.name, person.display.birthDate, person.display.ascendancyNumber, person.display.gender, "birth");
+          }
           }
           //Get marriage date
           if(person.display.marriageDate) {
-            // console.log("Marriage Date: " + new Date(person.display.marriageDate));
-            dates.push({'name': person.display.name, 'date': new Date(person.display.marriageDate), 'type': 'marriage'});
+            if(isValid(person.display.marriageDate)) {
+              addEventToCalendar(person.display.name, person.display.marriageDate, person.display.ascendancyNumber, person.display.gender, "marriage");
+            }
           }
           //Get death date
           if(person.display.deathDate) {
-            // console.log("Death Date: " + new Date(person.display.deathDate));
-            dates.push({'name': person.display.name, 'date': new Date(person.display.deathDate), 'type': 'death'});
+            if(isValid(person.display.deathDate)) {
+              addEventToCalendar(person.display.name, person.display.deathDate, person.display.ascendancyNumber, person.display.gender, "death");
+            }
           }
         })
-        // console.log("Dates: ", dates);
+        console.log("Calendar: ", JSON.stringify(calendar, null, 4));
         // Notify async.autoInject that we're done with this task and give it
         // the ancestry data so that the data is available for later tasks.
-        callback(null, dates);
+        callback(null, calendar);
       });
     }
-
   }, function(error, results){
-
-    // At this point async.autoInject has finished processing the tasks.
-    // If there's an error we pass it on to express which will display the error
-    // on the error page. If we successfully retrieved all the data then we
-    // pass the data on to the template. The results object will have the
-    // properties {ancestry, children} which are the results of the relative
-    // tasks above.
     if(error){
       next(error);
     } else {
-      console.log(results.dates);
-      res.render('calendar', {'dates': results.dates});
+      res.render('calendar', {'calendar': results.calendar});
     }
   });
 });
